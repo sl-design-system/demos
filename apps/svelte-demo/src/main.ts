7c3a5252b4a6bd549ec6b0b1de4816ff3f06f9f4
anna-lach <a.lach@nowaera.pl>
2025-07-30 15:50:28 +0200

    packages updated, svelte demo app improvements

--- apps/svelte-demo/src/main.ts
+++ apps/svelte-demo/src/main.ts
@@ -1,11 +1,16 @@
+import { mount } from "svelte";
 import App from './App.svelte';
 import '@sl-design-system/button/register.js';
 import { setup } from '@sl-design-system/sanoma-learning';
 
 setup();
 
-const app = new App({
+// const app = new App({
+//   target: document.body,
+// });
+//
+// export default app;
+
+export default mount(App, {
   target: document.body,
 });
-
-export default app;
