ae034f0991ef2abed044d62828f5f9cd2ec65031
Diana Broeders <info@dianabroeders.nl>
2025-09-10 16:25:16 +0200

    Add paginator and data source functionality to angular-demo app
    - Updated package.json to include @webcomponents/scoped-custom-element-registry and @sl-design-system/data-source dependencies.
    - Enhanced app.component.ts to implement data fetching and pagination.
    - Modified app.component.html to display quotes with pagination controls.
    - Updated app.module.ts to import PaginatorComponent.
    - Registered paginator component in main.ts.
    - Adjusted polyfills.ts for scoped custom element registry.

--- apps/angular-demo/src/app/app.component.ts
+++ apps/angular-demo/src/app/app.component.ts
@@ -1,11 +1,54 @@
+import { NgFor } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonComponent } from '@sl-design-system/angular/button';
+import { PaginatorComponent } from '@sl-design-system/angular/paginator';
+import { FetchListDataSource, FetchListDataSourceError, ListDataSourceItem } from '@sl-design-system/data-source';
 
+ interface Quote {
+      id: string;
+      quote: string;
+      author: string;
+    }
+
+    interface QuotesResponse {
+      quotes: Quote[];
+      total: number;
+      skip: number;
+      limit: number;
+    }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [ButtonComponent],
+  imports: [NgFor, ButtonComponent, PaginatorComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  
+  dataSource!: FetchListDataSource<Quote>;
+  quotes?:Quote[];
+
+  ngOnInit(): void {
+    this.dataSource = new FetchListDataSource<Quote>({
+      pageSize: 10,
+      pagination: true,
+      fetchPage: async ({ page, pageSize }) => {
+        const response = await fetch(`https://dummyjson.com/quotes?skip=${page * pageSize}&limit=${pageSize}`);
+
+        if (response.ok) {
+          const { quotes, total } = (await response.json()) as QuotesResponse;
+          this.quotes = quotes;
+          return { items: quotes, totalItems: total };
+        } else {
+          throw new FetchListDataSourceError('Failed to fetch data', response);
+        }
+      }
+    });
+
+  }
+  onPageChange() {
+    setTimeout(() => {
+       this.quotes = [...this.dataSource.items.map((item: ListDataSourceItem) => item.id as Quote)];
+    }, 10);
+  }
+}
