79e72f01ae4ac8de6f5402f18bb97eda0d304680
anna-lach <a.lach@nowaera.pl>
2025-12-19 08:18:15 +0100

    examples improvements

--- apps/angular-demo/src/app/nav-item.component.ts
+++ apps/angular-demo/src/app/nav-item.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { TooltipDirective } from '@sl-design-system/angular';
 
 @Component({
   selector: 'app-nav-item',
   standalone: true,
-  imports: [CommonModule/*, TooltipDirective*/],
+  imports: [CommonModule],
   templateUrl: './nav-item.component.html',
   styleUrls: ['./nav-item.component.css']
 })
@@ -14,17 +13,16 @@ export class NavItemComponent {
   @Input() href = '#';
   @Input() isActive = false;
   @Input() ariaDescribedby?: string;
-  // @Input() slTooltip?: string;
 
   onFocus(event: FocusEvent) {
     console.log('Focus event:', event, event.detail);
     const target = event.target as HTMLElement;
-    target.parentElement?.dispatchEvent(new FocusEvent('focus', { bubbles: true }));
+    target.parentElement?.focus();
   }
 
   onBlur(event: FocusEvent) {
     const target = event.target as HTMLElement;
-    target.parentElement?.dispatchEvent(new FocusEvent('blur', { bubbles: true }));
+    target.parentElement?.blur();
   }
 }
 
