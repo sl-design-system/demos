50a045d3fb03346eed16cb835587bec25aae6e9f
anna-lach <a.lach@nowaera.pl>
2026-01-13 13:37:05 +0100

    example with Angular Material button and a tooltip

--- apps/angular-demo/src/app/app.component.ts
+++ apps/angular-demo/src/app/app.component.ts
@@ -1,17 +1,56 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '@sl-design-system/angular/button';
 import { TooltipComponent } from '@sl-design-system/angular/tooltip';
 import { NavItemComponent } from './nav-item.component';
 import { NavLinkComponent } from './nav-link.component';
 import { TooltipDirective } from '@sl-design-system/angular';
+import { SelectComponent } from '@sl-design-system/angular/select';
+import { OptionComponent } from '@sl-design-system/angular/listbox';
+import { IconComponent } from '@sl-design-system/angular/icon';
+import { faCircle, faHexagonNodes, faTriangleExclamation, faSquare, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Icon } from '@sl-design-system/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [ButtonComponent, TooltipComponent, NavItemComponent, NavLinkComponent, TooltipDirective],
+  imports: [
+    ButtonComponent,
+    IconComponent,
+    TooltipComponent,
+    NavItemComponent,
+    NavLinkComponent,
+    SelectComponent,
+    TooltipDirective,
+    OptionComponent,
+    MatButtonModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   tooltipText = 'I am a tooltip added via directive';
+  link = signal('/home');
+  label = signal('Go Back');
+
+  @ViewChild('linkEl' , {read: ElementRef, static: true}) private linkEl! : ElementRef;
+
+  ngOnInit() {
+    Icon.register(faCircle, faHexagonNodes, faTriangleExclamation, faSquare, faArrowLeft);
+  }
+
+  onPointerover() {
+    this.linkEl.nativeElement.focus();
+  }
+
+  onPointerout() {
+    this.linkEl.nativeElement.blur();
+  }
 }
--- apps/angular-demo/src/app/app.module.ts
+++ apps/angular-demo/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from '@sl-design-system/angular/button';
 import { TooltipComponent } from '@sl-design-system/angular/tooltip';
 import { TooltipDirective } from '@sl-design-system/angular';
+import { SelectComponent } from '@sl-design-system/angular/select';
+import { OptionComponent } from '@sl-design-system/angular/listbox';
+import { IconComponent } from '@sl-design-system/angular/icon';
 
 @NgModule({
   declarations: [],
-  imports: [AppComponent, BrowserModule, ButtonComponent, TooltipComponent, TooltipDirective],
+  imports: [AppComponent, BrowserModule, ButtonComponent, IconComponent, SelectComponent, OptionComponent, TooltipComponent, TooltipDirective],
 })
 export class AppModule {}
