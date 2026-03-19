79ccb71b88e88b20ccfe560fc6a7545045b740c5
anna-lach <a.lach@nowaera.pl>
2026-03-19 15:27:26 +0100

    popover examples

--- apps/angular-demo/src/app/app.component.ts
+++ apps/angular-demo/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
   Component,
   CUSTOM_ELEMENTS_SCHEMA,
   ElementRef,
@@ -17,6 +20,7 @@ import { IconComponent } from '@sl-design-system/angular/icon';
 import { faCircle, faHexagonNodes, faTriangleExclamation, faSquare, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Icon } from '@sl-design-system/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { PopoverComponent } from '@sl-design-system/angular/popover';
 
 @Component({
   selector: 'app-root',
@@ -31,19 +35,40 @@ import { MatButtonModule } from '@angular/material/button';
     TooltipDirective,
     OptionComponent,
     MatButtonModule,
+    PopoverComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   tooltipText = 'I am a tooltip added via directive';
   link = signal('/home');
   label = signal('Go Back');
 
-  @ViewChild('linkEl' , {read: ElementRef, static: true}) private linkEl! : ElementRef;
+  @ViewChild('linkEl', { read: ElementRef, static: true })
+  private linkEl!: ElementRef;
+
+  @ViewChild('examplePopover', { read: ElementRef, static: true })
+  private examplePopover!: ElementRef;
+
+  @ViewChild('popoverEl', { read: ElementRef, static: false })
+  private popoverEl!: ElementRef;
 
   ngOnInit() {
-    Icon.register(faCircle, faHexagonNodes, faTriangleExclamation, faSquare, faArrowLeft);
+    Icon.register(
+      faCircle,
+      faHexagonNodes,
+      faTriangleExclamation,
+      faSquare,
+      faArrowLeft,
+    );
+  }
+
+  ngAfterViewInit() {
+    requestAnimationFrame(() => {
+      this.examplePopover.nativeElement.showPopover();
+      this.popoverEl.nativeElement.showPopover();
+    })
   }
 
   onPointerover() {
@@ -52,5 +77,13 @@ export class AppComponent implements OnInit {
 
   onPointerout() {
     this.linkEl.nativeElement.blur();
+  }
+
+  handleButtonClicked() {
+    this.popoverEl.nativeElement.showPopover();
+  }
+
+  handleCloseOnboardingPopover() {
+    this.popoverEl.nativeElement.hidePopover();
   }
 }
