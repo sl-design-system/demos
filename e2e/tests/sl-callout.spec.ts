92f86921caf417404ce00cf57803b97b63edcc0b
barszesan <bartosz.szerlag@sanoma.com>
2026-03-26 14:23:22 +0100

    Add e2e tests for sl-callout (#7)
    - add sl-callout component 
    - add e2e tests for sl-callout component
    - sort all components alphabetically

---
+++ e2e/tests/sl-callout.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.describe('sl-callout', () => {
+  let newPage: Page;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test.afterEach(async () => {
+    await newPage?.close();
+  });
+
+  test('should click the sl-callout link and open a new page', async ({
+    page,
+  }) => {
+    [newPage] = await Promise.all([
+      page.context().waitForEvent('page'),
+      page.locator('sl-callout a').click(),
+    ]);
+    await expect(newPage).toHaveURL('about:blank');
+  });
+
+  test('should click the sl-callout button and open a new page', async ({
+    page,
+  }) => {
+    [newPage] = await Promise.all([
+      page.context().waitForEvent('page'),
+      page.locator('sl-callout sl-button').click(),
+    ]);
+    await expect(newPage).toHaveURL('about:blank');
+  });
+});
