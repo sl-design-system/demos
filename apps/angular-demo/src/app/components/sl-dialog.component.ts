7410c041d7a6f260cb0b0baa65fcff7155e7c5d8
Bartosz Szerląg <bartosz.szerlag@sanoma.com>
2026-04-09 08:15:29 +0200

    fix angular sl-button

--- apps/angular-demo/src/app/components/sl-dialog.component.ts
+++ apps/angular-demo/src/app/components/sl-dialog.component.ts
@@ -1,17 +1,11 @@
-import {
-  AfterViewInit,
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonComponent } from '@sl-design-system/angular/button';
 import { DialogComponent } from '@sl-design-system/angular/dialog';
 
 @Component({
   selector: 'app-dialog-page',
   template: `
-    <sl-button #testBtn variant="primary" fill="solid">Test</sl-button>
+    <sl-button variant="primary" fill="solid" (click)="dialog.elRef.nativeElement.showModal()">Test</sl-button>
     <sl-dialog #dialog>
       <h1 slot="title">Test title</h1>
       Test description
@@ -21,13 +15,4 @@ import { DialogComponent } from '@sl-design-system/angular/dialog';
   imports: [ButtonComponent, DialogComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class DialogPageComponent implements AfterViewInit {
-  @ViewChild('testBtn', { read: ElementRef }) testBtn!: ElementRef;
-  @ViewChild('dialog', { read: ElementRef }) dialog!: ElementRef;
-
-  ngAfterViewInit(): void {
-    this.testBtn.nativeElement.addEventListener('click', () => {
-      this.dialog.nativeElement.showModal();
-    });
-  }
-}
+export class DialogPageComponent {}
