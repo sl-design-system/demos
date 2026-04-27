88475cd8a941b3e644f49028c791d5ea39464330
a11ymiko <kacper.mikocki@sanoma.com>
2026-04-27 14:05:34 +0200

    added e2e tests for sl-radio-group

--- e2e/tests/sl-radio-group.spec.ts
+++ e2e/tests/sl-radio-group.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('sl-radio-group', () => {
+  const errorMsg = 'Pick the middle option';
+
   test.beforeEach(async ({ page }) => {
     await page.goto('/sl-radio-group');
   });
@@ -8,13 +10,13 @@ test.describe('sl-radio-group', () => {
   test('should be clear on initial load', async ({
     page,
   }) => {
-    const radios = page.getByRole('radio');
-    for (const radio of await radios.all()) {
+    const activeRadios = page.getByRole('radio', { disabled: false });
+    for (const radio of await activeRadios.all()) {
       await expect(radio).not.toBeChecked();
     }
   });
 
-  test('should check radio group on click', async ({
+  test('should check radio button on click', async ({
     page,
   }) => {
     await page.getByRole('radio', { name: 'One' }).click();
@@ -23,14 +25,56 @@ test.describe('sl-radio-group', () => {
     await expect(page.getByRole('radio', { name: 'Three' })).not.toBeChecked();
   });
 
-  test('should close the menu when clicking outside', async ({ page }) => {
-    await page.getByRole('button', { name: 'Menu' }).click();
-    await expect(page.getByRole('menuitem', { name: 'Test 1' })).toBeVisible();
+  test('should deselect radio button after other is clicked', async ({
+    page,
+  }) => {
+    await page.getByRole('radio', { name: 'One' }).click();
+    await expect(page.getByRole('radio', { name: 'One' })).toBeChecked();
+    
+    await page.getByRole('radio', { name: 'Two' }).click();
+    await expect(page.getByRole('radio', { name: 'One' })).not.toBeChecked();
+    await expect(page.getByRole('radio', { name: 'Two' })).toBeChecked();
+  });
 
-    await page.mouse.click(10, 10);
+  test('should not show error when correct option is checked', async ({
+    page,
+  }) => {
+    await page.getByRole('radio', { name: 'Two' }).click();
+    await page.getByRole('button', { name: 'Report validity' }).click();
+    await expect(page.getByText(errorMsg)).not.toBeVisible();
+  });
 
-    await expect(
-      page.getByRole('menuitem', { name: 'Test 1' }),
-    ).not.toBeVisible();
+  test('should validate active radio group without any option checked', async ({
+    page,
+  }) => {
+    const activeRadios = page.getByRole('radio', { disabled: false });
+    for (const radio of await activeRadios.all()) {
+      await expect(radio).not.toBeChecked();
+    }
+    await page.getByRole('button', { name: 'Report validity' }).click();
+    await expect(page.getByText(errorMsg)).toBeVisible();
+  });
+
+  test('should hide validation message when no longer valid', async ({
+    page,
+  }) => {
+    await page.getByRole('radio', { name: 'One' }).click();
+    await page.getByRole('button', { name: 'Report validity' }).click();
+    await expect(page.getByText(errorMsg)).toBeVisible();
+
+    await page.getByRole('radio', { name: 'Two' }).click();
+    await page.getByRole('button', { name: 'Report validity' }).click();
+    await expect(page.getByText(errorMsg)).not.toBeVisible();
+  });
+
+  test('should not check any option in disabled radio group', async ({
+    page,
+  }) => {
+    const disabledRadios = page.getByRole('radio', { disabled: true });
+    for (const radio of await disabledRadios.all()) {
+      await expect(radio).toBeDisabled();
+      await radio.click({ force: true });
+      await expect(radio).not.toBeChecked();
+    }
   });
 });
