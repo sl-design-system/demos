f27ae61729642862843b31158a8edbfa4da3df9a
a11ymiko <kacper.mikocki@sanoma.com>
2026-05-09 12:47:55 +0200

    added formating

--- e2e/tests/a11y/a11y-sl-breadcrumbs.spec.ts
+++ e2e/tests/a11y/a11y-sl-breadcrumbs.spec.ts
@@ -7,14 +7,19 @@ test.describe('sl-breadcrumbs accessibility', () => {
     await page.goto('/sl-breadcrumbs');
   });
 
-  test('should have no accessibility violations in standard viewport', async ({ page }) => {
+  test('should have no accessibility violations in standard viewport', async ({
+    page,
+  }) => {
+    await page.setViewportSize({ width: 1200, height: 800 });
     await page.getByRole('button', { name: 'Collapse navigation' }).click();
     const axe = new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa']);
     const results = await axe.analyze();
     expect(results.violations).toEqual([]);
   });
 
-  test('should have no accessibility violations in mobile viewport', async ({ page }) => {
+  test('should have no accessibility violations in mobile viewport', async ({
+    page,
+  }) => {
     await page.setViewportSize({ width: 376, height: 667 }); // 320px width + 56px collapsed navigation
     await page.goto('/sl-breadcrumbs'); // for Firefox to properly apply the viewport size before page load
     await page.getByRole('button', { name: 'Collapse navigation' }).click();
@@ -38,24 +43,27 @@ test.describe('sl-breadcrumbs accessibility', () => {
 
   test(`should allow keyboard operability of home link`, async ({ page }) => {
     const baseUrl = '/sl-accordion';
-    await Promise.all([
-      page.waitForURL(baseUrl),
-      page.getByRole('link', { name: 'Home' }).press('Enter'),
-    ]);
+    const urlPromise = page.waitForURL(baseUrl);
+    await page.getByRole('link', { name: 'Home' }).press('Enter');
+    await urlPromise;
 
     await expect(page).toHaveURL(baseUrl);
   });
 
-  test(`should allow keyboard operability of current page link`, async ({ page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent('page'),
-      await page.getByRole('link', { name: 'Test 1' }).focus(),
-      await page.keyboard.press('Enter'),
-    ]);
+  test(`should allow keyboard operability of current page link`, async ({
+    page,
+  }) => {
+    const pagePromise = page.context().waitForEvent('page');
+    await page.getByRole('link', { name: 'Test 1' }).focus();
+    await page.keyboard.press('Enter');
+    const newPage = await pagePromise;
+
     await expect(newPage).toHaveURL('about:blank');
   });
 
-  test('should have aria-current attribute on the current page link', async ({ page }) => {
+  test('should have aria-current attribute on the current page link', async ({
+    page,
+  }) => {
     const item = page.getByRole('link', { name: 'Test 1' });
     await expect(item).toHaveAttribute('aria-current', 'page');
   });
@@ -65,9 +73,14 @@ test.describe('sl-breadcrumbs accessibility', () => {
     await expect(nav).toHaveAttribute('aria-label', 'Breadcrumb trail');
   });
 
-  test('should have aria-label when first link is icon-only', async ({ page }) => {
+  test('should have aria-label when first link is icon-only', async ({
+    page,
+  }) => {
     await page.setViewportSize({ width: 376, height: 667 });
     await page.goto('/sl-breadcrumbs'); // for Firefox to properly apply the viewport size before page load
-    await expect(page.getByRole('link', { name: 'Home' })).toHaveAttribute('aria-label', 'Home');
+    await expect(page.getByRole('link', { name: 'Home' })).toHaveAttribute(
+      'aria-label',
+      'Home',
+    );
   });
 });
