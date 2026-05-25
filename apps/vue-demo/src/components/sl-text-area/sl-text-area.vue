ee6f7aece7b4d2b35e6fd86168bda1020dfd1363
a11ymiko <kacper.mikocki@sanoma.com>
2026-05-25 16:26:33 +0200

    add sl-text-area to apps in Angular, Lit, Svelte and Vue

---
+++ apps/vue-demo/src/components/sl-text-area/sl-text-area.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-area-container">
+    <sl-text-area
+      aria-label="Text area"
+      placeholder="Type your message"
+      rows="4"
+    ></sl-text-area>
+  </div>
+</template>
+
+<script setup lang="ts">
+import '@sl-design-system/text-area/register.js';
+</script>
+
+<style scoped lang="scss" src="./sl-text-area.scss"></style>
