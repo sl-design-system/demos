7c3e16d339fc1b17a7d712799b039f325b1cb252
a11ymiko <kacper.mikocki@sanoma.com>
2026-09-01 09:37:08 +0200

    add a11y tests for sl-inline-message

---
+++ e2e/tests/a11y/a11y-sl-inline-message.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { hasMainHorizontalOverflow } from '../../utils/checkForHorizontalScroll.js';
+import { getFocusedElement } from '../../utils/getFocusedElement.js';
+
+test.describe('sl-inline-message accessibility', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/sl-inline-message');
+  });
+
+  test('should have no accessibility violations', async ({ page }) => {
+    const axe = new AxeBuilder({ page }).withTags([
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22a',
+      'wcag22aa',
+    ]);
+    const results = await axe.analyze();
+    expect(results.violations).toEqual([]);
+  });
+
+  test('should have no accessibility violations in mobile viewport', async ({
+    page,
+  }) => {
+    await page.setViewportSize({ width: 376, height: 667 }); // 320px width + 56px collapsed navigation
+    await page.goto('/sl-inline-message'); // for Firefox to properly apply the viewport size before page load
+    await page.getByRole('button', { name: 'Collapse navigation' }).click();
+
+    const axe = new AxeBuilder({ page }).withTags([
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22a',
+      'wcag22aa',
+    ]);
+    const results = await axe.analyze();
+    expect(results.violations).toEqual([]);
+  });
+
+  test('component fits without horizontal scroll', async ({ page }) => {
+    await page.setViewportSize({ width: 376, height: 667 }); // 320px width + 56px collapsed navigation
+    await page.goto('/sl-inline-message'); // for Firefox to properly apply the viewport size before page load
+    await page.getByRole('button', { name: 'Collapse navigation' }).click();
+
+    const hasOverflow = await hasMainHorizontalOverflow(page);
+    expect(hasOverflow).toBe(false);
+  });
+
+  test('should have correct tab order', async ({ page }) => {
+      const activeElements = ['Close'] as const;
+
+      await page.getByRole('button', { name: 'Collapse navigation' }).click();
+
+      for (const activeElement of activeElements) {
+        await page.keyboard.press('Tab');
+        const focusedOn = await getFocusedElement(page);
+        expect(focusedOn).toBe(activeElement);
+      }
+  });
+
+  test(`should be activated and closed with Space key`, async ({ page }) => {
+    const item = page.getByRole('button', { name: 'Close' });
+
+    await item.focus();
+    await page.keyboard.press('Space');
+    await expect(item).not.toBeVisible();
+  });
+});
