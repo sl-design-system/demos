5512559a35d591f7033bd6c398350a72fb3b6ce7
a11ymiko <kacper.mikocki@sanoma.com>
2026-09-10 15:45:28 +0200

    add a11y tests for sl-text-field

---
+++ e2e/tests/a11y/a11y-sl-text-field.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { hasMainHorizontalOverflow } from '../../utils/checkForHorizontalScroll.js';
+import { getFocusedElement } from '../../utils/getFocusedElement.js';
+
+test.describe('sl-text-field accessibility', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/sl-text-field');
+  });
+
+  test('should have no accessibility violations', async ({ page }) => {
+    await expect(
+      page.getByRole('textbox', { name: 'Text field', exact: true }),
+    ).toBeVisible();
+    const axe = new AxeBuilder({ page }).withTags([
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22a',
+      'wcag22aa',
+    ]);
+    const results = await axe.analyze();
+    expect(results.violations).toEqual([]);
+  });
+
+  test('should have no accessibility violations in 320px width of <main>', async ({
+    page,
+  }) => {
+    await page.setViewportSize({ width: 376, height: 667 }); // 320px width + 56px collapsed navigation
+    await page.goto('/sl-text-field'); // for Firefox to properly apply the viewport size before page load
+    await page.getByRole('button', { name: 'Collapse navigation' }).click();
+    const axe = new AxeBuilder({ page }).withTags([
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22a',
+      'wcag22aa',
+    ]);
+    const results = await axe.analyze();
+    expect(results.violations).toEqual([]);
+  });
+
+  test('component fits without horizontal scroll', async ({ page }) => {
+    await page.setViewportSize({ width: 376, height: 667 }); // 320px width + 56px collapsed navigation
+    await page.goto('/sl-text-field'); // for Firefox to properly apply the viewport size before page load
+    await page.getByRole('button', { name: 'Collapse navigation' }).click();
+
+    const hasOverflow = await hasMainHorizontalOverflow(page);
+    expect(hasOverflow).toBe(false);
+  });
+
+  test('should have accessible name', async ({ page }) => {
+    const item = page.getByRole('textbox', { name: 'Text field', exact: true });
+    await expect(item).toHaveAccessibleName('Text field');
+  });
+
+  test('should have correct tab order', async ({ page }) => {
+    const activeElements = ['Text field', 'Focus me'] as const;
+
+    await page.getByRole('button', { name: 'Collapse navigation' }).click();
+
+    for (const activeElement of activeElements) {
+      await page.keyboard.press('Tab');
+      const focusedOn = await getFocusedElement(page);
+      expect(focusedOn).toBe(activeElement);
+    }
+  });
+
+  test(`should be keyboard operable`, async ({ page }) => {
+    const item = page.getByRole('textbox', { name: 'Text field', exact: true });
+
+    await item.focus();
+    await page.keyboard.type('Hello world');
+    await expect(item).toHaveValue('Hello world');
+  });
+});
